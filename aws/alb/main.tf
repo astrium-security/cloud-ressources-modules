58faedc3acf11b1a4a7705cf2d1c05505b40623c
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-07-30 12:47:32 +0200

    modules

--- aws/alb/main.tf
+++ aws/alb/main.tf
@@ -63,7 +63,7 @@ resource "aws_lb_listener" "app_redirect" {
 
 resource "aws_lb_target_group" "tg-app" {
   name        = "${var.prefix}-${var.container_name}-${var.app_environment}-tg-${random_id.unique_id.hex}"
-  port        = var.container_port
+  port        = 80
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = var.vpc_id
