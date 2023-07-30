7ca9b5e1f4676f73fb170c09fcd531a6999388f9
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-07-30 02:06:38 +0200

    modules

--- aws/container_app_deployment/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "security_group_id" {
-  description = "The ID of the security group"
-  value       = module.security_groups.security_group_id
-}
--- aws/standalone_resources/security_group/outputs.tf
+++ aws/standalone_resources/security_group/outputs.tf
@@ -1,4 +1,4 @@
-output "security_group" {
+output "id" {
   description = "The ID of the security group"
-  value       = aws_security_group.sg
+  value       = aws_security_group.sg.id
 }
