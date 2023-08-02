d7a269f3ef0849c3f85ece16f99f97ba2de85621
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-03 01:18:27 +0200

    modules

--- aws/init_tunnel/main.tf
+++ aws/init_tunnel/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.5.2"
+    required_providers {
+      cloudflare = {
+        source  = "registry.terraform.io/cloudflare/cloudflare"
+        version = "~> 4.10.0"
+      }
+    }
+}
+
 data "cloudflare_origin_ca_root_certificate" "origin_ca" {
   algorithm = "RSA"
 }
