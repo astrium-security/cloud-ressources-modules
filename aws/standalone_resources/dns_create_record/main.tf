bdee635aec4e39bf4873824151c6fe79391f028b
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-03 01:26:56 +0200

    modules

--- aws/standalone_resources/dns_create_record/main.tf
+++ aws/standalone_resources/dns_create_record/main.tf
@@ -8,10 +8,10 @@ resource "aws_route53_record" "internal_record" {
   records = var.targets
 }
 
-resource "cloudflare_record" "example" {
-  zone_id = var.cloudflare_zone_id
-  name    = "${var.app_name}.${var.app_env}.${var.prefix}.${var.region}.${var.cloudflare_zone_id.name}"
-  value   = var.targets
-  type    = var.type_record
-  ttl     = 3600
-}
+#resource "cloudflare_record" "example" {
+#  zone_id = var.cloudflare_zone_id
+#  name    = "${var.app_name}.${var.app_env}.${var.prefix}.${var.region}.${var.cloudflare_zone_id.name}"
+#  value   = var.targets
+#  type    = var.type_record
+#  ttl     = 3600
+#}
