f5552ffcdeb349038dec3f16605fc5659a879bf3
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-02 18:16:55 +0200

    modules

--- aws/standalone_resources/ec2_instance/main.tf
+++ aws/standalone_resources/ec2_instance/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "simple-instance" {
   ami                         = var.default_ami 
   instance_type               = var.instance_type 
   
-  subnet_id                   = var.subnet
+  subnet_id                   = var.subnet_id
   vpc_security_group_ids      = var.security_group
   
   associate_public_ip_address = var.set_public_ip_address
