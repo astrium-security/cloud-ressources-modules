150e87cfb364e1f3573463ab546defe93e708016
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-03 20:30:01 +0200

    modules

--- aws/standalone_resources/ecs_task_definition/main.tf
+++ aws/standalone_resources/ecs_task_definition/main.tf
@@ -11,10 +11,11 @@ resource "aws_ecs_task_definition" "main_app" {
     name      = "efs"
     efs_volume_configuration {
       file_system_id = aws_efs_file_system.efs.id
-      creation_info {
-        owner_gid = 1000
-        uid = 1000
-        permissions = 775
+      transit_encryption = "ENABLED"
+
+      authorization_config {
+        access_point_id = aws_efs_access_point.this.id
+        iam             = "ENABLED"
       }
     }
   }
@@ -47,6 +48,79 @@ resource "aws_ecs_task_definition" "main_app" {
   }])
 }
 
+resource "aws_efs_access_point" "this" {
+  file_system_id = aws_efs_file_system.efs.id
+
+  posix_user {
+    gid = 1000
+    uid = 1000
+  }
+
+  root_directory {
+    path = "${var.mount_efs}"
+
+    creation_info {
+      owner_gid   = 1000
+      owner_uid   = 1000
+      permissions = 755
+    }
+  }
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    actions = [
+      "elasticfilesystem:ClientMount",
+      "elasticfilesystem:ClientWrite"
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      aws_efs_file_system.efs.arn,
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["true"]
+    }
+  }
+}
+
+resource "aws_efs_file_system_policy" "this" {
+  file_system_id = aws_efs_file_system.efs.id
+  policy = data.aws_iam_policy_document.this.json
+}
+
+data "aws_iam_policy_document" "efs_access" {
+  statement {
+    actions = [
+      "elasticfilesystem:ClientMount",
+      "elasticfilesystem:ClientWrite"
+    ]
+
+    resources = [
+      aws_efs_file_system.efs.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "efs_access" {
+  name   = "${var.prefix}-${var.container_name}-${var.app_environment}-efs-access"
+  policy = data.aws_iam_policy_document.efs_access.json
+}
+
+resource "aws_iam_role_policy_attachment" "efs_access" {
+  role       = aws_iam_role.task.name
+  policy_arn = aws_iam_policy.efs_access.arn
+}
+
 resource "aws_iam_role" "ecs_task_execution_role_app" {
   name = "${var.prefix}-${var.container_name}-${var.app_environment}-ecsTaskExec"
 
@@ -71,12 +145,21 @@ resource "aws_efs_file_system" "efs" {
   performance_mode = "generalPurpose"
 
   creation_token = "${var.prefix}-${var.container_name}-${var.app_environment}-volume"
+  
   lifecycle_policy {
     transition_to_ia = "AFTER_7_DAYS"
   }
 
   tags = {
     Name = "${var.prefix}-${var.container_name}-${var.app_environment}-volume"
+  }
+}
+
+resource "aws_efs_backup_policy" "policy" {
+  file_system_id = aws_efs_file_system.efs.id
+
+  backup_policy {
+    status = "ENABLED"
   }
 }
 
