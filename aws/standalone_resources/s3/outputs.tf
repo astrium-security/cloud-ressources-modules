2050ad0b272d48a2525d6d34925db2d3415e8e98
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-11 15:55:54 +0200

    modules

--- aws/standalone_resources/s3/outputs.tf
+++ aws/standalone_resources/s3/outputs.tf
@@ -3,6 +3,12 @@ output "s3_bucket_name" {
   value       = aws_s3_bucket.bucket.bucket
 }
 
+output "s3_bucket_id" {
+  description = "The name of the created S3 bucket"
+  value       = aws_s3_bucket.bucket.id
+}
+
+
 output "s3_bucket_arn" {
   description = "The ARN of the created S3 bucket"
   value       = aws_s3_bucket.bucket.arn
