8f05832007422d6ed720d092d51a92f66aa5c94b
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-12 14:06:36 +0200

    modules

--- aws/alb/main.tf
+++ aws/alb/main.tf
@@ -106,8 +106,16 @@ resource "aws_security_group" "lb_sg_app" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = 80
+    to_port     = 80
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming HTTP connections"
+  }
+
+   ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow incoming HTTP connections"
