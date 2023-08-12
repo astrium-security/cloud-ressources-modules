f470184323b2176419a06cce4212a90356bd08b2
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-12 20:53:10 +0200

    modules

--- aws/cloudtrail/outputs.tf
+++ aws/cloudtrail/outputs.tf
@@ -1,8 +1,3 @@
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket used for cloudtrail logs"
-  value       = aws_s3_bucket.cloudtrail_logs.bucket
-}
-
 output "cloudtrail_name" {
   description = "The name of the CloudTrail"
   value       = aws_cloudtrail.main.name
