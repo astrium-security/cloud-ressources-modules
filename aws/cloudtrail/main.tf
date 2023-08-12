1bbd137321405afabad2b7bbdfe121198075f1d7
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-12 21:07:36 +0200

    modules

--- aws/cloudtrail/main.tf
+++ aws/cloudtrail/main.tf
@@ -30,9 +30,9 @@ resource "aws_cloudtrail" "main" {
   include_global_service_events = true 
   is_multi_region_trail         = true 
 
-  enable_log_file_validation = true
-  cloud_watch_logs_group_arn = aws_cloudwatch_log_group.cloudtrail.arn
-  cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_cloudwatch.arn
+  #-enable_log_file_validation = true
+  #cloud_watch_logs_group_arn = aws_cloudwatch_log_group.cloudtrail.arn
+  #cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_cloudwatch.arn
 }
 
 resource "aws_cloudwatch_log_group" "cloudtrail" {
