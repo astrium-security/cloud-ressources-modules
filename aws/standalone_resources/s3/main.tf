c241d53af4371c2e1af314b391d7184ee6359d6f
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-13 23:12:05 +0200

    modules

--- aws/standalone_resources/s3/main.tf
+++ aws/standalone_resources/s3/main.tf
@@ -69,7 +69,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_cloudtrail" "write_event_trail" {
-  name           = "${s3_bucket_name}"
+  name           = "${aws_s3_bucket.bucket.bucket}"
   s3_bucket_name = aws_s3_bucket.bucket.bucket
 
   event_selector {
