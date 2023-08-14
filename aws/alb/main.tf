2787dae8d54580dbf08226e423dee02dd3948c05
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-14 19:30:59 +0200

    modules

--- aws/alb/main.tf
+++ aws/alb/main.tf
@@ -132,20 +132,20 @@ resource "aws_security_group" "lb_sg_app" {
 module "kms_s3_key" {
     source                  = "../standalone_resources/kms"
     prefix                  = var.prefix
-    app_environment         = var.infra_environment
-    description             = "${var.prefix}-${var.container_name}-${var.app_environment}"
+    app_environment         = var.app_environment
+    description             = "${var.container_name}"
     deletion_window_in_days = 7
     enable_key_rotation     = false
     multi_region            = false
-    key_name                = "${var.prefix}-${var.container_name}-${var.app_environment}"
+    key_name                = "${var.container_name}"
 }
 
 module "my_s3_bucket" {
   source = "../standalone_resources/s3"
 
   prefix           = var.prefix
-  app_environment  = var.infra_environment
-  name             = "${var.prefix}-${var.container_name}-${var.app_environment}"
+  app_environment  = var.app_environment
+  name             = "${var.container_name}"
   kms_key_arn      = module.kms_s3_key.kms_key_arn
   block_public_acls   = true
   block_public_policy = true
