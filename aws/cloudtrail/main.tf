dcfd4c12b3115d85ddba640508c6fbdb51678450
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-14 22:53:49 +0200

    modules

--- aws/cloudtrail/main.tf
+++ aws/cloudtrail/main.tf
@@ -120,3 +120,83 @@ resource "aws_s3_bucket_policy" "cloudtrail_policy" {
 }
 
 data "aws_caller_identity" "current" {}
+
+locals {
+  regions = [
+    "us-east-1",
+    "us-east-2",
+    "us-west-1",
+    "us-west-2",
+    "eu-west-1",
+    "eu-west-2",
+    "eu-west-3",
+    "eu-central-1",
+    "ap-southeast-1",
+    "ap-southeast-2",
+    "ap-northeast-1",
+    "ap-northeast-2",
+    "ap-south-1",
+    "ca-central-1",
+    "sa-east-1",
+    "eu-north-1",
+    "me-south-1",
+    "ap-east-1",
+    "af-south-1"
+  ]
+}
+
+resource "aws_config_configuration_recorder" "this" {
+  for_each = { for region in local.regions : region => region }
+
+  name     = "default"
+  role_arn = aws_iam_role.config.arn
+
+  recording_group {
+    all_supported                 = true
+    include_global_resource_types = true
+  }
+}
+
+module "config_bucket" {
+  source = "../standalone_resources/s3"
+
+  prefix           = var.prefix
+  app_environment  = var.infra_environment
+  name             = "config"
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+}
+
+resource "aws_config_delivery_channel" "this" {
+  for_each = { for region in local.regions : region => region }
+
+  name           = "default"
+  s3_bucket_name = module.config_bucket.s3_bucket_name
+  s3_key_prefix  = "config"
+  snapshot_delivery_properties {
+    delivery_frequency = "TwentyFour_Hours"
+  }
+}
+
+resource "aws_iam_role" "config" {
+  name = "awsconfig-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "config.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "aws_config" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
+  role       = aws_iam_role.config.name
+}
