ae6e3bde70193891addd14bf407bd727e705bb7f
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-30 18:02:59 +0200

    s3 buckets fix

--- aws/standalone_resources/s3/main.tf
+++ aws/standalone_resources/s3/main.tf
@@ -75,25 +75,27 @@ resource "aws_s3_bucket_policy" "target_bucket_policy" {
   bucket = aws_s3_bucket.bucket.id
 
   policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "AllowCloudTrailBucketLogging",
-        Effect    = "Allow",
-        Principal = {
-          Service = "cloudtrail.amazonaws.com"
-        },
-        Action   = ["s3:GetBucketAcl", "s3:PutObject"],
-        Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.bucket.bucket}",
-          "arn:aws:s3:::${aws_s3_bucket.bucket.bucket}/*"
-        ],
-        Condition = {
-          StringEquals = {
-            "s3:x-amz-acl" = "bucket-owner-full-control"
-          }
-        }
+    Sid       = "AllowCloudTrailBucketLoggingPut",
+    Effect    = "Allow",
+    Principal = {
+      Service = "cloudtrail.amazonaws.com"
+    },
+    Action   = "s3:PutObject",
+    Resource = "arn:aws:s3:::${aws_s3_bucket.bucket.bucket}/*",
+    Condition = {
+      StringEquals = {
+        "s3:x-amz-acl" = "bucket-owner-full-control"
       }
-    ]
-  })
+    }
+  },
+  {
+    Sid       = "AllowCloudTrailBucketLoggingGetAcl",
+    Effect    = "Allow",
+    Principal = {
+      Service = "cloudtrail.amazonaws.com"
+    },
+    Action   = "s3:GetBucketAcl",
+    Resource = "arn:aws:s3:::${aws_s3_bucket.bucket.bucket}"
+  }
+  )
 }
