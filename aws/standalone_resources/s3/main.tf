9f309375d58facc4cb552dc2c4dd8cc2c7ea39c2
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-30 16:09:44 +0200

    s3 buckets fix

--- aws/standalone_resources/s3/main.tf
+++ aws/standalone_resources/s3/main.tf
@@ -13,10 +13,10 @@ resource "aws_s3_bucket" "log_bucket" {
   acl           = "private"
 }
 
-resource "aws_s3_bucket_acl" "log_bucket_acl" {
-  bucket = aws_s3_bucket.log_bucket.id
-  acl    = "log-delivery-write"
-}
+#resource "aws_s3_bucket_acl" "log_bucket_acl" {
+#  bucket = aws_s3_bucket.log_bucket.id
+#  acl    = "log-delivery-write"
+#}
 
 resource "aws_s3_bucket_logging" "b_logging" {
   bucket = aws_s3_bucket.bucket.id
