7bdfe96e361c9d66628ba596c011d832427504d2
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-30 16:23:07 +0200

    s3 buckets fix

--- aws/standalone_resources/s3/main.tf
+++ aws/standalone_resources/s3/main.tf
@@ -22,10 +22,10 @@ resource "aws_s3_bucket_ownership_controls" "example" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.log_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_acl" "write-log" {
@@ -36,16 +36,6 @@ resource "aws_s3_bucket_acl" "write-log" {
 
   bucket = aws_s3_bucket.log_bucket.id
   acl    = "log-delivery-write"
-}
-
-resource "aws_s3_bucket_acl" "read-log" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.example,
-    aws_s3_bucket_public_access_block.example,
-  ]
-
-  bucket = aws_s3_bucket.log_bucket.id
-  acl    = "log-delivery-read"
 }
 
 resource "aws_s3_bucket_logging" "b_logging" {
