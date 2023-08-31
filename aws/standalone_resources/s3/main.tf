dbf16d1404e68f6cd08e358ab862f2416b03d478
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-31 17:06:45 +0200

    s3 buckets fix

--- aws/standalone_resources/s3/main.tf
+++ aws/standalone_resources/s3/main.tf
@@ -72,36 +72,3 @@ resource "aws_s3_bucket_logging" "b_logging" {
   target_prefix = "log/"
 }
 
-resource "aws_cloudtrail" "s3_object_read_logger" {
-  name                          = "${var.prefix}-${var.app_environment}-${var.name}-${random_id.name_suffix.hex}-read-logger"
-  s3_bucket_name                = "${var.prefix}-${var.app_environment}-cloudtrail-${data.aws_caller_identity.current.account_id}"
-  include_global_service_events = true
-
-  event_selector {
-    read_write_type           = "ReadOnly"
-    include_management_events = true
-
-    data_resource {
-      type = "AWS::S3::Object"
-
-      values = ["${aws_s3_bucket.bucket.arn}/"]
-    }
-  }
-}
-
-resource "aws_cloudtrail" "s3_object_write_logger" {
-  name                          = "${var.prefix}-${var.app_environment}-${var.name}-${random_id.name_suffix.hex}-write-logger"
-  s3_bucket_name                = "${var.prefix}-${var.app_environment}-cloudtrail-${data.aws_caller_identity.current.account_id}"
-  include_global_service_events = true
-
-  event_selector {
-    read_write_type           = "WriteOnly"
-    include_management_events = true
-
-    data_resource {
-      type = "AWS::S3::Object"
-
-      values = ["${aws_s3_bucket.bucket.arn}/"]
-    }
-  }
-}
