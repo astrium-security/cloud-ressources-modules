01e27da9e2b093055cbdf058a7235b607b73384c
Sacha IFRAH <sachaifrah@mbp-de-sacha-2.home>
2024-02-06 23:34:12 +0100

    s

--- aws/rds_scalable_aurora/variables.tf
+++ aws/rds_scalable_aurora/variables.tf
@@ -9,29 +9,43 @@ variable "vpc" {
         vpc_id = string
         subnets = any
     })
+    default = {
+      vpc_id = "vpc-0a52c88a170901671"
+      subnets = [
+        {
+            id = "subnet-090b70a700c6e2b80"
+            cidr_block = "172.31.0.0/20"
+        }
+      ]
+    }
 }
 
 variable "min_acu_capacity" {
     description = "min_acu_capacity"
     type        = number
+    default = 1
 }
 
 variable "max_acu_capacity" {
     description = "max_acu_capacity"
     type        = number
+    default = 1
 }
 
 variable "min_capacity" {
     description = "min_capacity"
     type        = number
+    default = 1
 }
 
 variable "max_capacity" {
     description = "min_capacity"
     type        = number
+    default = 1
 }
 
 variable "business_hours_only" {
     description = "business_hours_only"
     type        = bool
+    default = true
 }
